5e757d8fa3c72300a7ac6fd1885b5f2431c55331
Théfa <140509878+Thefa07@users.noreply.github.com>
2024-04-04 11:29:04 -0300

    Add files via upload

---
+++ and.js
@@ -0,0 +1,7 @@
+let idade = 25;
+let pssuiCarteiraDeMotorista = true;
+if (idade >= 10 && pssuiCarteiraDeMotorista) {
+    console.log("você pode dirigir.");
+}else{
+    console.log("você não pode dirigir.");
+}
