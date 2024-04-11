55db7d8a2d6283e21c5710d2fac7777ffa93fe3e
Théfa <140509878+Thefa07@users.noreply.github.com>
2024-04-11 11:45:06 -0300

    Create template-strings-2.js

---
+++ template-strings-2.js
@@ -0,0 +1,7 @@
+const nome = "Sthéfanie";
+const idade = 2024 = 2006;
+const cidade = "Curitiba";
+
+const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + ", e nasci na cidade de " + cidade + ".";
+
+console.log(apresentacao)
