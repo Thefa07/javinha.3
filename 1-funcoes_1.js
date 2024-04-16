d68357908f6d51efd1ab775214c97d04c638b8cd
Théfa <140509878+Thefa07@users.noreply.github.com>
2024-04-16 09:41:00 -0300

    Create 1-funcoes_1.js

---
+++ 1-funcoes_1.js
@@ -0,0 +1,11 @@
+function imprimeTexto(texto){
+    console.log(texto);
+}
+
+imprimeTexto("Fala pra mim o que você é?")
+imprimeTexto("Eu sou uma rata senhora!")
+imprimeTexto("E sabe o que a rata vai fazer agora?")
+imprimeTexto("o que senhora?")
+imprimeTexto("vai me carregar, sua rata!!!!")
+
+
